f4bd5f0b79a1852f4e1237073ff8532adf5e6755
Sadie Staudacher <sadiestaudacher@Sadies-MacBook-Pro.local>
2018-04-19 19:44:23 -0400

    adding final steps of project

--- SI364final.py
+++ SI364final.py
@@ -19,7 +19,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364thisisnotsupersecurebutitsok'
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or "postgresql://localhost/SI364projectplanSADIES"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or "postgresql://localhost/SI364finalSADIES"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -38,6 +38,7 @@ login_manager.init_app(app) # set up login manager
 #spotify api
 client_id = '2efb491c8f8b49e6825224f3491a1e6e'
 client_secret = '828d2c8abb9f407489ea390451d5d9fb'
+oauth_token = 'BQDrRkCnJa63yIZJx_vQafrU3vqGcqZ7tqOz1SBaIZz7ZgbHOTOZtv_ID6ZTJ5duqUbM7DpWL-ixnfmM_JuY2M_nFm-8KH4bKtepTCyImh5JpWP_pCU13ak7n0yexsfFji6q-oFcyU2XcQJlG_09Zg1vgYj1zi_DgQ0'
 
 #########################
 ##### Set up Models #####
@@ -69,11 +70,18 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id)) # returns User object or None
+
 # Playlist model
 class Playlist(db.Model):
     __tablename__ = "playlists"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64),unique=True)
+    spotify_id = db.Column(db.String())
+    user_id = db.Column(db.String())
     pictureURL = db.Column(db.String(256))
     reviews = db.relationship('PlaylistReviews', backref='Playlist') # one playlist can have many reviews, but the same review cannot be used for other playlists (one to many relationship)
 
@@ -84,9 +92,9 @@ class PlaylistReviews(db.Model):
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
     review = db.Column(db.String(256))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    username = db.Column(db.String, db.ForeignKey('users.username'))
     stars = db.Column(db.Integer())
-    playlist = db.Column(db.Integer, db.ForeignKey("playlists.id"))
+    playlist = db.Column(db.String, db.ForeignKey("playlists.title"))
 
 class SearchTerm(db.Model):
     __tablename__ = 'search_term'
@@ -130,30 +138,39 @@ class PlaylistSearchForm(FlaskForm):
 # Leave a review
 class LeaveReviewForm(FlaskForm):
     review = StringField("Enter a review for this playlist", validators=[Required(),Length(max=300, message="The review cannot be longer than 300 characters!")])
-    stars = IntegerField("Rate the movie out of 5 stars ",validators=[Required()])
+    stars = IntegerField("Rate the playlist out of 5 stars ",validators=[Required()])
     submit = SubmitField("Submit your review")
 
     def validate_stars(form, field):
         if len(str(field.data)) > 1 :
             raise ValidationError('Have to be full numbers, no decimals for star rating')
 
-# Update
-# Delete
+class UpdateButtonForm(FlaskForm):
+    submit = SubmitField('Update')
 
 
+class DeleteButtonForm(FlaskForm):
+    submit = SubmitField('Delete')
 
+
+class UpdateReviewForm(FlaskForm):
+    new_review = StringField("What your new review for this playlist?", validators=[Required(),Length(max=300, message="The review cannot be longer than 300 characters!")])
+    new_stars = IntegerField("Rate the playlist out of 5 stars ",validators=[Required()])
+    submit = SubmitField('Update')
+
+    def validate_new_stars(form, field):
+        if len(str(field.data)) > 1 :
+            raise ValidationError('Have to be full numbers, no decimals for star rating')
 
 
 ##### Helper functions
-### For database additions / get_or_create functions
-#in this function, you will pass in a term/phrase of what type/kind of playlist you are looking for. it will then return a search object with a dictionary of playlist information
 def spotify_search(term):
-    oauth_token = "BQD9dTfBQLNX2Ci2HyLSRME9vCIlmtFuYPZFPVZhNkxrv1E0DOTPsEJNFu8gtOxBDfaLS9Xb51NZJ4RMpvmx7CP1ZN7NzFJQKQYLzI61FZc0B85Fzgv32YomBLxLlB2oA3i5Fs56VWDE7Le6A_QO-cMy1j98wFKWrjY" #oauth token for spotify
     headers ={"Content-Type": "application/json", "Authorization": "Bearer " + oauth_token}
     params = { 'q': term, 'type': 'playlist', 'limit':'10'} #q is any term that will be used to get a playlsit
     search_object = requests.get('https://api.spotify.com/v1/search?', headers=headers, params = params).json() #search object
     playlists = search_object['playlists']['items'] #get the playlist items
     return(playlists)
+
 
 #this function will take in a term and then use the get_spotify function to recieve list of tupples for each playlist. each tupple will contain a playlist's name, id, and user_id
 def list_of_playlist_tupples(term):
@@ -167,12 +184,12 @@ def list_of_playlist_tupples(term):
         list_of_playlist_info.append((playlist_name,playlist_id, user_id, pictureURL))
     return(list_of_playlist_info)
 
+
 #this function will use a tupple (containing playlist name, id, and user_id) and make another request to spotify to get the tracks and artists for each playlist
-def get_playlist_songs_and_artist(tupple):
+def get_playlist_songs_and_artist(user_id, spotify_id):
     songs_and_artists = []
-    oauth_token = "BQAVhLvaDkhPII_qNBURcrjL0B4T-038NOG5i96cttghdFZnrzlABEueS--puYg33Yx0op6ccX7YNTHjc3gGv26VAkD2H0XtOqv7blYzM3tQ3JxeHWcEd77XAbog_Aap5CMWQiWmnY15oNd65PlbFFc3WdKLYqpolTI" #you need to generate the OAuth token from https://beta.developer.spotify.com/console/get-search-item/
     headers ={"Content-Type": "application/json", "Authorization": "Bearer " + oauth_token}
-    search_object = requests.get('https://api.spotify.com/v1/users/'+tupple[2]+'/playlists/'+tupple[1]+'/tracks', headers=headers).json()['items']#[0]['track']
+    search_object = requests.get('https://api.spotify.com/v1/users/'+user_id+'/playlists/'+spotify_id+'/tracks', headers=headers).json()['items']
     for item in search_object:
         song = item['track']['name']
         all_artists = item['track']['artists']
@@ -185,12 +202,12 @@ def get_playlist_songs_and_artist(tupple):
     return(songs_and_artists)
 
 
-def get_or_create_playlist(title, pictureURL):
+def get_or_create_playlist(title, spotify_id, user_id, pictureURL):
     playlist = Playlist.query.filter_by(title=title).first()
     if playlist:
         return playlist
     else:
-        playlist = Playlist(title=title, pictureURL=pictureURL)
+        playlist = Playlist(title=title, spotify_id=spotify_id, user_id = user_id, pictureURL=pictureURL)
         db.session.add(playlist)
         db.session.commit()
         return playlist
@@ -206,15 +223,27 @@ def get_or_create_search_term(term):
         playlists_search = list_of_playlist_tupples(term)
         for playlist in playlists_search:
             title=playlist[0]
+            spotify_id = playlist[1]
+            user_id = playlist[2]
             pictureURL = playlist[3]
-            new_playlist = get_or_create_playlist(title, pictureURL)
+            new_playlist = get_or_create_playlist(title, spotify_id, user_id, pictureURL)
             search_term.playlists.append(new_playlist)
         db.session.add(search_term)
         db.session.commit()
         return search_term
 
 
-# def get_or_create_review ?????
+def get_or_create_review(current_user, playlist, review, stars):
+    playlist_review = PlaylistReviews.query.filter_by(review=review, username = current_user.username).first()
+    if playlist_review:
+        print("Found review")
+        return playlist_review
+    else:
+        print("Adding review")
+        playlist_review = PlaylistReviews(review=review, username = current_user.username, stars = stars, playlist = playlist)
+        db.session.add(playlist_review)
+        db.session.commit()
+        return playlist_review
 
 
 
@@ -271,32 +300,74 @@ def playlist_results(search_term):
     return render_template('searched_playlists.html', playlists = relevant_playlists, term = term)
 
 @app.route('/playlist_songs/<playlist_name>')
+# @login_required
 def playlist_songs(playlist_name):
-    pass #this will show the user the songs in a certain playlist. they will also be able to see reviews if a user has left any on this playlist. if they are logged in, they will be able to leave a review this will render url for leave_review it will use get_playlist_songs_and_artist
-    # return render_template('playlist_songs.html')
+    playlist = Playlist.query.filter_by(title=playlist_name).first()
+    playlist_tupple = get_playlist_songs_and_artist(playlist.user_id, playlist.spotify_id)
+    reviews = PlaylistReviews.query.filter_by(playlist = playlist.title).all()
+    number_of_songs = str(len(playlist_tupple))
+    return render_template('playlist_songs.html', playlist_tupple=playlist_tupple, playlist_name = playlist.title, reviews = reviews, number_of_songs=number_of_songs)
 
-# Provided
+
+@app.route('/leave_review/<playlist_name>',methods=["GET","POST"])
+@login_required
+def leave_review(playlist_name):
+    form = LeaveReviewForm()
+    if request.method == "POST":
+        playlist_review = get_or_create_review(current_user=current_user, playlist=playlist_name, review=form.review.data, stars=form.stars.data)
+        return redirect(url_for('all_reviews'))
+    return render_template('leave_review.html', form=form, playlist_name = playlist_name)
+
+
 @app.route('/all_reviews')
+@login_required
 def all_reviews():
-    pass #this will show you all reviews that have been left by users
-    # return render_template('all_reviews.html',all_reviews=reviews)
+    reviews = PlaylistReviews.query.all()
+    return render_template('all_reviews.html',all_reviews=reviews)
 
 @app.route('/searched_terms')
 def all_searches():
     all_terms = SearchTerm.query.all()
     return render_template('search_terms.html', all_terms = all_terms)
 
-@app.route('/leave_review',methods=["GET","POST"])
+
+@app.route('/my_reviews')
 @login_required
-def leave_review():
-    pass #a user is required to be logged in to leave a review. they will be able to leave a review and after leaving it will redirect url_for playlist reviews. it will render template for leave_review.html
+def my_reviews():
+    form1 = UpdateButtonForm()
+    form2 = DeleteButtonForm()
+    user_reviews = PlaylistReviews.query.filter_by(username=current_user.username).all()
+    return render_template('user_reviews.html', user_reviews=user_reviews, form1 = form1, form2=form2)
 
+@app.route('/update/<review>',methods=["GET","POST"])
+@login_required
+def update(review):
+    form = UpdateReviewForm()
+    if form.validate_on_submit():
+        new_review = form.new_review.data
+        new_stars = form.new_stars.data
+        playlist = PlaylistReviews.query.filter_by(review=review).first()
+        playlist.review = new_review
+        playlist.stars = new_stars
+        db.session.commit()
+        flash("Updated review")
+        return redirect(url_for('my_reviews'))
+    return render_template('update_review.html', review = review, form=form)
 
-# @app.route('/my_reviews')
-# @login_required
-# def my_reviews():
-#     pass #this view functionw will render a template to allow a user to view their own reviews for playlists
-#     # return render_template('my_reviews.html', reviews=my_reviews)
+@app.route('/delete/<review>',methods=["GET","POST"])
+@login_required
+def delete(review):
+    playlist_review = PlaylistReviews.query.filter_by(review=review).first()
+    playlist_review_title = playlist_review.playlist
+    db.session.delete(playlist_review)
+    flash("Successfully deleted your review for: {}".format(playlist_review_title))
+    return redirect(url_for('my_reviews'))
+
+@app.route('/all_users')
+@login_required
+def all_users():
+    all_users = User.query.all()
+    return render_template('all_users.html',all_users=all_users)
 
 @app.errorhandler(404)
 def page_not_found(e):
